56793b405414cdf9776d6e15121a30a691256467
Christy Perozzi <christy.perozzi@gmail.com>
2021-11-30 19:59:26 -0500

    completed

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i-- ){
+    newStr = newStr + str.charAt(i); 
+  }
+  return newStr; 
 }
 
 if (require.main === module) {
@@ -11,9 +16,24 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: 'ti ni secaps htiw'");
+  console.log("=>", reverseString("with spaces in it"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+ 
+  //loop backwards from str.lenth to 0; --
+  // for each i find the charAt(i)
+  // set a new string to ""
+  // newString = newString + charAt(i)
+
 // And a written explanation of your solution
+    /*
+      I looped backwards from the end of the string to the beginning and 
+      concatinated each character onto a new string 
+    */
